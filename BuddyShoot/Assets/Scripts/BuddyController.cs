b6c5f0edae62aae3551254ee550bcd49044e5cb4
YangYoungHwan00 <qjddldi17@gmail.com>
2024-05-24 10:54:55 +0900

    add character tag

--- BuddyShoot/Assets/Scripts/BuddyController.cs
+++ BuddyShoot/Assets/Scripts/BuddyController.cs
@@ -4,14 +4,52 @@ using UnityEngine;
 
 public class BuddyController : MonoBehaviour
 {
+    public float speed = 10f;
+    public string name = "maru";
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
+    {   
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CharacterTag();
+        }
+        
+    }
+
+    void FixedUpdate() 
     {
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(0,speed*Time.deltaTime,0);
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(0,-speed*Time.deltaTime,0);
+        if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(-speed*Time.deltaTime,0,0);
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(speed*Time.deltaTime,0,0);
+    }
+
+    void CharacterTag()
+    {
+        GameObject a = transform.GetChild(0).gameObject;
+        GameObject b = transform.GetChild(1).gameObject;
+        
+        if(a.activeSelf)
+        {
+            a.SetActive(false);
+            b.SetActive(true);
+            Debug.Log("good");
+        }
+        else
+        {
+            a.SetActive(true);
+            b.SetActive(false);
+            Debug.Log("bad");
+        }
     }
 }
